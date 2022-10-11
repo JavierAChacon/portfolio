13ea3e3a16e3d0c60b7f1787d4c90e8dd6ad954c
JavierChacon <alejandro.chr@outlook.es>
2022-10-11 02:06:13 -0400

    added the links buttons

--- src/components/LandPage.jsx
+++ src/components/LandPage.jsx
@@ -6,17 +6,26 @@ import perfil from '../assets/images/red.jpg'
 const LandPage = () => {
   return (
     <div className='tittlePage'>
-      
+
       <div className='tittle'>
-        <div className='name'>
-          <span>I'm Javier Chacon a</span>
+        <div className='text'>
+          <div className='name'>
+            <span>I'm Javier Chacon a</span>
+          </div>
+          <h1>Front-End <br /> Developer</h1>
         </div>
-        
-        <h1>Front-End <br/> Developer</h1>
+
+        <div className="buttons">
+          <ul className='listButtons'>
+            <li><a className="buttonText" href="https://github.com/JavierAChacon"><i className="fa-brands fa-github" aria-hidden="true"></i></a></li>
+            <li><a className="buttonText" href="https://www.linkedin.com/in/javier-a-chacon/"><i className="fa-brands fa-linkedin-in" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
 
       </div>
 
-      <div className='imagen'><img  src={perfil} alt="" /></div>
+      <div className='imagen'><img src={perfil} alt="" /></div>
+
     </div>
   )
 }
